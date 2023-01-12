9903cc0dc755cf3d566c8f7d4373bd8ad04aa838
sreekanthpsree <cnct2sree@yahoo.com>
2023-01-12 23:17:53 +0530

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [result, setResult] = useState("");
+  const clickHandler = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+  const clear = () => {
+    setResult("");
+  };
+  const del = () => {
+    setResult(result.slice(0, result.length - 1));
+  };
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="output">
+          <div className="previouseValue"></div>
+          <div className="currentValue">{result}</div>
+          {/* <input id="text" type="text" value={result} /> */}
+        </div>
+        <div className="keypad">
+          <button id="clear" onClick={clear}>
+            AC
+          </button>
+          <button className="symbols" id="del" onClick={del}>
+            DEL
+          </button>
+
+          <button className="symbols" name="/" onClick={clickHandler}>
+            &divide;
+          </button>
+          <button name="7" onClick={clickHandler}>
+            7
+          </button>
+          <button name="8" onClick={clickHandler}>
+            8
+          </button>
+          <button name="9" onClick={clickHandler}>
+            9
+          </button>
+          <button className="symbols" name="*" onClick={clickHandler}>
+            &times;
+          </button>
+          <button name="4" onClick={clickHandler}>
+            4
+          </button>
+          <button name="5" onClick={clickHandler}>
+            5
+          </button>
+          <button name="6" onClick={clickHandler}>
+            6
+          </button>
+          <button className="symbols" name="-" onClick={clickHandler}>
+            -
+          </button>
+          <button name="1" onClick={clickHandler}>
+            1
+          </button>
+          <button name="2" onClick={clickHandler}>
+            2
+          </button>
+          <button name="3" onClick={clickHandler}>
+            3
+          </button>
+          <button className="symbols" name="+" onClick={clickHandler}>
+            +
+          </button>
+          <button name="0" onClick={clickHandler}>
+            0
+          </button>
+          <button name="." onClick={clickHandler}>
+            .
+          </button>
+          <button id="result" onClick={calculate}>
+            =
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
